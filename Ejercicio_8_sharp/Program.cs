5ca1defac10ee6d7e046c22102c93826a04993c2
Meliodas-R <inceptiongodmaxwell@gmail.com>
2021-10-28 11:30:52 +0200

    Agregadas las peticiones por teclado, la función para obtener la posicion del caracter y el muestreo por pantalla.

--- Ejercicio_8_sharp/Program.cs
+++ Ejercicio_8_sharp/Program.cs
@@ -6,7 +6,21 @@ namespace Ejercicio_8_sharp
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Introduzca un texto: ");
+            string texto = Console.ReadLine();
+            Console.WriteLine("\nPrimer caracter de la palabra: " + texto[0]);
+
+            Console.Write("\nIntroduzca la posicion del caracter que desea visualizar:  ");
+            int indice = Convert.ToInt32(Console.ReadLine()) - 1;
+
+            if (indice < texto.Length)
+            {
+                Console.WriteLine("La letra situada en la posición [" + (indice + 1) + "] es: " + texto[indice]);
+            }
+            else
+            {
+                Console.WriteLine("La posicion introducida es incorrecta.");
+            }
         }
     }
 }
